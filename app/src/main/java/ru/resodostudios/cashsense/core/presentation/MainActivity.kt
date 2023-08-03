2ee328549d94393ca486a8f3f0d90f97e4815cbe
Nikita Bulavin <f33lnothin9@proton.me>
2023-08-03 20:25:11 +0300

    Update Material Theme

--- app/src/main/java/ru/resodostudios/cashsense/core/presentation/MainActivity.kt
+++ app/src/main/java/ru/resodostudios/cashsense/core/presentation/MainActivity.kt
@@ -1,16 +1,19 @@
-package ru.resodostudios.cashsense
+package ru.resodostudios.cashsense.core.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import ru.resodostudios.cashsense.ui.theme.CashSenseTheme
+import androidx.core.view.WindowCompat
+import ru.resodostudios.cashsense.core.presentation.theme.CashSenseTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
         setContent {
             CashSenseTheme {
-
+                
             }
         }
     }
