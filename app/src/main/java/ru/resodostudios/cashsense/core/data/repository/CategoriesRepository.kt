3c72e1f9a2b29e5128dfa149561226f40f0ecb8c
Nikita Bulavin <f33lnothin9@proton.me>
2023-08-19 12:53:19 +0300

    Create CategoriesRepository.kt

---
+++ app/src/main/java/ru/resodostudios/cashsense/core/data/repository/CategoriesRepository.kt
@@ -0,0 +1,15 @@
+package ru.resodostudios.cashsense.core.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import ru.resodostudios.cashsense.core.model.data.Category
+
+interface CategoriesRepository {
+
+    fun getCategory(categoryId: String): Flow<Category>
+
+    fun getCategories(): Flow<List<Category>>
+
+    suspend fun upsertCategory(category: Category)
+
+    suspend fun deleteCategory(category: Category)
+}
