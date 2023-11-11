509b51fe54f6993ce4bd2de9577fba8372b067da
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-11 17:28:30 +0300

    Delete firebase mapping file uploading

--- build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationFirebaseConventionPlugin.kt
@@ -1,9 +1,5 @@
-
-import com.android.build.api.dsl.ApplicationExtension
-import com.google.firebase.crashlytics.buildtools.gradle.CrashlyticsExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import ru.resodostudios.cashsense.libs
 
@@ -22,17 +18,6 @@ class AndroidApplicationFirebaseConventionPlugin : Plugin<Project> {
                 "implementation"(libs.findLibrary("firebase.analytics").get())
                 "implementation"(libs.findLibrary("firebase.performance").get())
                 "implementation"(libs.findLibrary("firebase.crashlytics").get())
-            }
-
-            extensions.configure<ApplicationExtension> {
-                buildTypes.configureEach {
-                    // Disable the Crashlytics mapping file upload. This feature should only be
-                    // enabled if a Firebase backend is available and configured in
-                    // google-services.json.
-                    configure<CrashlyticsExtension> {
-                        mappingFileUploadEnabled = false
-                    }
-                }
             }
         }
     }
