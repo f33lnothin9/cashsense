d8ddd294b87aee50659b2c3edb38d6eca1d7bd8b
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-17 19:14:35 +0300

    Create UserDataRepository.kt

---
+++ core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/repository/UserDataRepository.kt
@@ -0,0 +1,14 @@
+package ru.resodostudios.cashsense.core.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import ru.resodostudios.cashsense.core.model.data.DarkThemeConfig
+import ru.resodostudios.cashsense.core.model.data.UserData
+
+interface UserDataRepository {
+
+    val userData: Flow<UserData>
+
+    suspend fun setDarkThemeConfig(darkThemeConfig: DarkThemeConfig)
+
+    suspend fun setDynamicColorPreference(useDynamicColor: Boolean)
+}
