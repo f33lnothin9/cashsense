d3efb563843a0ad8ca76f69d088b3c00b93738bc
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-18 11:46:14 +0300

    Create MainActivityViewModel.kt

---
+++ app/src/main/java/ru/resodostudios/cashsense/MainActivityViewModel.kt
@@ -0,0 +1,32 @@
+package ru.resodostudios.cashsense
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import ru.resodostudios.cashsense.MainActivityUiState.Loading
+import ru.resodostudios.cashsense.MainActivityUiState.Success
+import ru.resodostudios.cashsense.core.data.repository.UserDataRepository
+import ru.resodostudios.cashsense.core.model.data.UserData
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    userDataRepository: UserDataRepository,
+) : ViewModel() {
+    val uiState: StateFlow<MainActivityUiState> = userDataRepository.userData.map {
+        Success(it)
+    }.stateIn(
+        scope = viewModelScope,
+        initialValue = Loading,
+        started = SharingStarted.WhileSubscribed(5_000),
+    )
+}
+
+sealed interface MainActivityUiState {
+    data object Loading : MainActivityUiState
+    data class Success(val userData: UserData) : MainActivityUiState
+}
