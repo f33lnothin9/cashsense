d9fc38dfa8271d61455d714e171e183341bc97b3
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-19 13:01:41 +0300

    Split strings by feature

--- app/src/main/java/ru/resodostudios/cashsense/navigation/TopLevelDestination.kt
+++ app/src/main/java/ru/resodostudios/cashsense/navigation/TopLevelDestination.kt
@@ -2,6 +2,9 @@ package ru.resodostudios.cashsense.navigation
 
 import ru.resodostudios.cashsense.R
 import ru.resodostudios.cashsense.core.designsystem.icon.CsIcons
+import ru.resodostudios.cashsense.feature.categories.R as categoriesR
+import ru.resodostudios.cashsense.feature.home.R as homeR
+import ru.resodostudios.cashsense.feature.subscriptions.R as subscriptionsR
 
 enum class TopLevelDestination(
     val selectedIcon: Int,
@@ -12,19 +15,19 @@ enum class TopLevelDestination(
     HOME(
         selectedIcon = CsIcons.HomeFilled,
         unselectedIcon = CsIcons.Home,
-        iconTextId = R.string.home,
+        iconTextId = homeR.string.home,
         titleTextId = R.string.app_name,
     ),
     CATEGORIES(
         selectedIcon = CsIcons.CategoryFilled,
         unselectedIcon = CsIcons.Category,
-        iconTextId = R.string.categories,
-        titleTextId = R.string.categories,
+        iconTextId = categoriesR.string.categories,
+        titleTextId = categoriesR.string.categories,
     ),
     SUBSCRIPTIONS(
         selectedIcon = CsIcons.SubscriptionsFilled,
         unselectedIcon = CsIcons.Subscriptions,
-        iconTextId = R.string.subscriptions,
-        titleTextId = R.string.subscriptions,
+        iconTextId = subscriptionsR.string.subscriptions,
+        titleTextId = subscriptionsR.string.subscriptions,
     )
 }
