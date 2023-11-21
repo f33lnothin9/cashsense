726df60378faaaa007302f3103075a02de32ddb5
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-21 19:09:16 +0300

    Add WalletDao

---
+++ core/database/src/main/kotlin/ru/resodostudios/cashsense/core/database/dao/WalletDao.kt
@@ -0,0 +1,29 @@
+package ru.resodostudios.cashsense.core.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+import ru.resodostudios.cashsense.core.database.model.WalletEntity
+
+@Dao
+interface WalletDao {
+
+    @Query(
+        value = """
+        SELECT * FROM wallets
+        WHERE id = :walletId
+    """
+    )
+    fun getWalletEntity(walletId: String): Flow<WalletEntity>
+
+    @Query(value = "SELECT * FROM wallets ORDER BY id DESC")
+    fun getWalletEntities(): Flow<List<WalletEntity>>
+
+    @Upsert
+    suspend fun upsertWallet(wallet: WalletEntity)
+
+    @Delete
+    suspend fun deleteWallet(wallet: WalletEntity)
+}
