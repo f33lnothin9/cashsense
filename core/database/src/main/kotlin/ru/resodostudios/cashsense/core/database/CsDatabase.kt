74801b40a12ea13dddc8707237d72bfb273072a8
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-21 19:15:54 +0300

    Add WalletDao to CsDatabase

--- core/database/src/main/kotlin/ru/resodostudios/cashsense/core/database/CsDatabase.kt
+++ core/database/src/main/kotlin/ru/resodostudios/cashsense/core/database/CsDatabase.kt
@@ -5,14 +5,17 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import ru.resodostudios.cashsense.core.database.dao.CategoryDao
 import ru.resodostudios.cashsense.core.database.dao.TransactionDao
+import ru.resodostudios.cashsense.core.database.dao.WalletDao
 import ru.resodostudios.cashsense.core.database.model.CategoryEntity
 import ru.resodostudios.cashsense.core.database.model.TransactionEntity
+import ru.resodostudios.cashsense.core.database.model.WalletEntity
 import ru.resodostudios.cashsense.core.database.util.LocalDateTimeConverter
 
 @Database(
     entities = [
         TransactionEntity::class,
-        CategoryEntity::class
+        CategoryEntity::class,
+        WalletEntity::class
     ],
     version = 1,
     exportSchema = true
@@ -23,4 +26,6 @@ abstract class CsDatabase : RoomDatabase() {
     abstract fun transactionDao(): TransactionDao
 
     abstract fun categoryDao(): CategoryDao
+
+    abstract fun walletDao(): WalletDao
 }
