df245a0bfb80466dcdc4b45c61650717fb5adec8
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-22 21:33:37 +0300

    Create OfflineWalletsRepository.kt

---
+++ core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/repository/offline/OfflineWalletsRepository.kt
@@ -0,0 +1,28 @@
+package ru.resodostudios.cashsense.core.data.repository.offline
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import ru.resodostudios.cashsense.core.data.model.asEntity
+import ru.resodostudios.cashsense.core.data.repository.WalletsRepository
+import ru.resodostudios.cashsense.core.database.dao.WalletDao
+import ru.resodostudios.cashsense.core.database.model.WalletEntity
+import ru.resodostudios.cashsense.core.database.model.asExternalModel
+import ru.resodostudios.cashsense.core.model.data.Wallet
+import javax.inject.Inject
+
+class OfflineWalletsRepository @Inject constructor(
+    private val walletDao: WalletDao
+) : WalletsRepository {
+
+    override fun getWallet(walletId: String): Flow<Wallet> =
+        walletDao.getWalletEntity(walletId).map { it.asExternalModel() }
+
+    override fun getWallets(): Flow<List<Wallet>> =
+        walletDao.getWalletEntities().map { it.map(WalletEntity::asExternalModel) }
+
+    override suspend fun upsertWallet(wallet: Wallet) =
+        walletDao.upsertWallet(wallet.asEntity())
+
+    override suspend fun deleteWallet(wallet: Wallet) =
+        walletDao.deleteWallet(wallet.asEntity())
+}
