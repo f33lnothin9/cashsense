ff8545fe78dc251a8f1b9de7c8e6700f5c336991
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-22 21:33:14 +0300

    Add Wallet asEntity()

---
+++ core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/model/Wallet.kt
@@ -0,0 +1,13 @@
+package ru.resodostudios.cashsense.core.data.model
+
+import ru.resodostudios.cashsense.core.database.model.WalletEntity
+import ru.resodostudios.cashsense.core.model.data.Wallet
+
+fun Wallet.asEntity() = WalletEntity(
+    id = id,
+    title = title,
+    startBalance = startBalance,
+    currency = currency,
+    income = income,
+    expenses = expenses
+)
