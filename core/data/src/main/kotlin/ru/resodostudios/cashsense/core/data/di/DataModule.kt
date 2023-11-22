30900c6e3913f018e6682da24a65c9255d7d7d27
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-22 21:50:12 +0300

    Bind walletsRepository

--- core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/di/DataModule.kt
+++ core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/di/DataModule.kt
@@ -7,9 +7,11 @@ import dagger.hilt.components.SingletonComponent
 import ru.resodostudios.cashsense.core.data.repository.CategoriesRepository
 import ru.resodostudios.cashsense.core.data.repository.TransactionsRepository
 import ru.resodostudios.cashsense.core.data.repository.UserDataRepository
+import ru.resodostudios.cashsense.core.data.repository.WalletsRepository
 import ru.resodostudios.cashsense.core.data.repository.offline.OfflineCategoriesRepository
 import ru.resodostudios.cashsense.core.data.repository.offline.OfflineTransactionsRepository
 import ru.resodostudios.cashsense.core.data.repository.offline.OfflineUserDataRepository
+import ru.resodostudios.cashsense.core.data.repository.offline.OfflineWalletsRepository
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -24,6 +26,11 @@ interface DataModule {
     fun bindTransactionsRepository(
         offlineTransactionsRepository: OfflineTransactionsRepository
     ): TransactionsRepository
+
+    @Binds
+    fun bindWalletsRepository(
+        offlineWalletsRepository: OfflineWalletsRepository
+    ): WalletsRepository
 
     @Binds
     fun bindUserDataRepository(
