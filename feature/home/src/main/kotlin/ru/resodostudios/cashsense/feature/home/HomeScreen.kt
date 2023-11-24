ce3501055b4725d492b5f9f27348975d2891d6b3
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-24 22:35:00 +0300

    Add wallet list

--- feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/HomeScreen.kt
+++ feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/HomeScreen.kt
@@ -1,11 +1,20 @@
 package ru.resodostudios.cashsense.feature.home
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyGridScope
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import ru.resodostudios.cashsense.core.model.data.Wallet
 import ru.resodostudios.cashsense.core.ui.EmptyState
 import ru.resodostudios.cashsense.core.ui.LoadingState
+import ru.resodostudios.cashsense.feature.wallets.WalletCard
 import ru.resodostudios.cashsense.feature.wallets.WalletsUiState
 import ru.resodostudios.cashsense.feature.wallets.WalletsViewModel
 import ru.resodostudios.cashsense.feature.wallets.R as walletsR
@@ -27,12 +36,29 @@ internal fun HomeScreen(
     when (walletsState) {
         WalletsUiState.Loading -> LoadingState()
         is WalletsUiState.Success -> if (walletsState.wallets.isNotEmpty()) {
-
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(300.dp),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                wallets(
+                    wallets = walletsState.wallets,
+                    onEdit = { TODO() }
+                )
+            }
         } else {
             EmptyState(
                 messageId = walletsR.string.wallets_empty,
                 animationId = walletsR.raw.anim_wallet_empty
             )
         }
+    }
+}
+
+private fun LazyGridScope.wallets(
+    wallets: List<Wallet>,
+    onEdit: (Wallet) -> Unit
+) {
+    items(wallets) { wallet ->
+        WalletCard(wallet = wallet)
     }
 }
