c61a11d5b038fd3299dcd659fdabdccbcc29365b
Nikita Bulavin <f33lnothin9@proton.me>
2023-11-28 19:20:15 +0300

    Add transaction dialog to home screen

--- feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/HomeScreen.kt
+++ feature/home/src/main/kotlin/ru/resodostudios/cashsense/feature/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package ru.resodostudios.cashsense.feature.home
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -9,6 +10,10 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -16,20 +21,21 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import ru.resodostudios.cashsense.core.model.data.Wallet
 import ru.resodostudios.cashsense.core.ui.EmptyState
 import ru.resodostudios.cashsense.core.ui.LoadingState
+import ru.resodostudios.cashsense.feature.transactions.TransactionDialog
+import ru.resodostudios.cashsense.feature.wallets.R
 import ru.resodostudios.cashsense.feature.wallets.WalletCard
 import ru.resodostudios.cashsense.feature.wallets.WalletsUiState
 import ru.resodostudios.cashsense.feature.wallets.WalletsViewModel
-import ru.resodostudios.cashsense.feature.wallets.R as walletsR
 
 @Composable
 internal fun HomeRoute(
-    walletViewModel: WalletsViewModel = hiltViewModel()
+    walletsViewModel: WalletsViewModel = hiltViewModel()
 ) {
-    val walletsState by walletViewModel.walletsUiState.collectAsStateWithLifecycle()
+    val walletsState by walletsViewModel.walletsUiState.collectAsStateWithLifecycle()
     HomeScreen(
         walletsState = walletsState,
         onEdit = { TODO() },
-        onDelete = walletViewModel::deleteWallet
+        onDelete = walletsViewModel::deleteWallet
     )
 }
 
@@ -39,28 +45,43 @@ internal fun HomeScreen(
     onEdit: (Wallet) -> Unit,
     onDelete: (Wallet) -> Unit
 ) {
-    when (walletsState) {
-        WalletsUiState.Loading -> LoadingState()
-        is WalletsUiState.Success -> if (walletsState.wallets.isNotEmpty()) {
-            LazyVerticalGrid(
-                columns = GridCells.Adaptive(300.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize()
-            ) {
-                wallets(
-                    wallets = walletsState.wallets,
-                    onTransactionCreate = { TODO() },
-                    onEdit = onEdit,
-                    onDelete = onDelete
+    var showNewTransactionDialog by rememberSaveable { mutableStateOf(false) }
+    var walletId by rememberSaveable { mutableIntStateOf(0) }
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        when (walletsState) {
+            WalletsUiState.Loading -> LoadingState()
+            is WalletsUiState.Success -> if (walletsState.wallets.isNotEmpty()) {
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(300.dp),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .fillMaxSize()
+                ) {
+                    wallets(
+                        wallets = walletsState.wallets,
+                        onTransactionCreate = {
+                            walletId = it     
+                            showNewTransactionDialog = true
+                        },
+                        onEdit = onEdit,
+                        onDelete = onDelete
+                    )
+                }
+            } else {
+                EmptyState(
+                    messageId = R.string.wallets_empty,
+                    animationId = R.raw.anim_wallet_empty
                 )
             }
-        } else {
-            EmptyState(
-                messageId = walletsR.string.wallets_empty,
-                animationId = walletsR.raw.anim_wallet_empty
+        }
+        if (showNewTransactionDialog) {
+            TransactionDialog(
+                onDismiss = { showNewTransactionDialog = false }
             )
         }
     }
