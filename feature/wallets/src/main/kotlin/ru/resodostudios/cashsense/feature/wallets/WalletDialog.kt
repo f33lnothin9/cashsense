28d15593bb6b071f8fd2baf6c68045d91e9e8ed4
Nikita Bulavin <f33lnothin9@proton.me>
2023-12-01 20:18:48 +0300

    Add currency dropdown menu

--- feature/wallets/src/main/kotlin/ru/resodostudios/cashsense/feature/wallets/WalletDialog.kt
+++ feature/wallets/src/main/kotlin/ru/resodostudios/cashsense/feature/wallets/WalletDialog.kt
@@ -1,10 +1,14 @@
 package ru.resodostudios.cashsense.feature.wallets
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -13,8 +17,11 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -83,8 +90,8 @@ fun NewWalletDialog(
                         keyboardType = KeyboardType.Text
                     ),
                     trailingIcon = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(CsIcons.DropDown, contentDescription = null)
+                        CurrencyDropDownMenu {
+                            currency = it
                         }
                     },
                     readOnly = true,
@@ -116,4 +123,33 @@ fun NewWalletDialog(
             }
         }
     )
+}
+
+@Composable
+fun CurrencyDropDownMenu(
+    onCurrencyClick: (Currency) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier.wrapContentSize(Alignment.TopStart)
+    ) {
+        IconButton(onClick = { expanded = true }) {
+            Icon(CsIcons.DropDown, contentDescription = null)
+        }
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
+        ) {
+            for (currency in Currency.entries) {
+                DropdownMenuItem(
+                    text = { Text(text = currency.name) },
+                    onClick = {
+                        onCurrencyClick(currency)
+                        expanded = false
+                    }
+                )
+            }
+        }
+    }
 }
