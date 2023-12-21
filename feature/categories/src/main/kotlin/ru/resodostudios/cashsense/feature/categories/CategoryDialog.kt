44025160964a1355464b53ae72c8137e149dfa87
Nikita Bulavin <f33lnothin9@proton.me>
2023-12-21 23:00:39 +0300

    Replace category alert dialog

--- feature/categories/src/main/kotlin/ru/resodostudios/cashsense/feature/categories/CategoryDialog.kt
+++ feature/categories/src/main/kotlin/ru/resodostudios/cashsense/feature/categories/CategoryDialog.kt
@@ -3,23 +3,20 @@ package ru.resodostudios.cashsense.feature.categories
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Category
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import ru.resodostudios.cashsense.core.designsystem.icon.CsIcons
+import ru.resodostudios.cashsense.core.designsystem.component.CsAlertDialog
 import ru.resodostudios.cashsense.core.model.data.Category
 import ru.resodostudios.cashsense.core.ui.R as uiR
 
@@ -28,7 +25,6 @@ fun CategoryDialog(
     onDismiss: () -> Unit,
     viewModel: CategoriesViewModel = hiltViewModel()
 ) {
-
     CategoryDialog(
         onDismiss = onDismiss,
         onConfirm = {
@@ -45,50 +41,33 @@ fun CategoryDialog(
 ) {
     var title by rememberSaveable { mutableStateOf("") }
 
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        icon = {
-            Icon(
-                painter = painterResource(CsIcons.Category),
-                contentDescription = null
+    CsAlertDialog(
+        titleRes = R.string.new_category,
+        confirmButtonTextRes = uiR.string.add,
+        dismissButtonTextRes = uiR.string.cancel,
+        icon = Icons.Outlined.Category,
+        onConfirm = {
+            onConfirm(
+                Category(
+                    title = title,
+                    icon = null
+                )
             )
         },
-        title = { Text(text = stringResource(R.string.new_category)) },
-        text = {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                TextField(
-                    value = title,
-                    onValueChange = { title = it },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Text
-                    ),
-                    label = { Text(text = stringResource(R.string.name)) },
-                    maxLines = 1
-                )
-            }
-        },
-        confirmButton = {
-            Button(
-                onClick = {
-                    onConfirm(
-                        Category(
-                            title = title,
-                            icon = null
-                        )
-                    )
-                }
-            ) {
-                Text(text = stringResource(uiR.string.save))
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = onDismiss
-            ) {
-                Text(text = stringResource(uiR.string.cancel))
-            }
+        onDismiss = onDismiss
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            TextField(
+                value = title,
+                onValueChange = { title = it },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text
+                ),
+                label = { Text(text = stringResource(R.string.name)) },
+                maxLines = 1
+            )
         }
-    )
+    }
 }
