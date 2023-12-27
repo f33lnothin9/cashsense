8a61cca5b40a694f011209153cb276f6e6481995
Nikita Bulavin <f33lnothin9@proton.me>
2023-12-27 22:36:37 +0300

    Update splash icon

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -13,8 +13,6 @@ android {
 dependencies {
     //lintPublish(projects.lint)
 
-    //api(libs.androidx.compose.foundation)
-    //api(libs.androidx.compose.foundation.layout)
     api(libs.androidx.compose.material.iconsExtended)
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.runtime)
