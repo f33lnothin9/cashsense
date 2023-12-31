b74f63f73abdaaf78b2e4fa73c35a35703d8137b
Nikita Bulavin <f33lnothin9@proton.me>
2023-12-31 09:29:03 +0300

    Bump app version to 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
 
     defaultConfig {
         applicationId = "ru.resodostudios.cashsense"
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
