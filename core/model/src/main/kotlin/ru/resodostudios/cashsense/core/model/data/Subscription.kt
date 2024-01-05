51745bb62a80610b987958709102a0fe2b2078a6
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-05 22:37:59 +0300

    Add subscriptionId to Subscription class

--- core/model/src/main/kotlin/ru/resodostudios/cashsense/core/model/data/Subscription.kt
+++ core/model/src/main/kotlin/ru/resodostudios/cashsense/core/model/data/Subscription.kt
@@ -2,8 +2,10 @@ package ru.resodostudios.cashsense.core.model.data
 
 import kotlinx.datetime.Instant
 import java.math.BigDecimal
+import java.util.UUID
 
 data class Subscription(
+    val subscriptionId: UUID = UUID.randomUUID(),
     val title: String,
     val amount: BigDecimal,
     val paymentDate: Instant,
