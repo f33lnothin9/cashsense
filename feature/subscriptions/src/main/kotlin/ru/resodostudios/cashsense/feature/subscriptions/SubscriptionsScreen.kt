9feb404f5f1e7e2530d0695d2aa1573215a58487
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-06 14:30:07 +0300

    Add subscriptionsState to SubscriptionsScreen

--- feature/subscriptions/src/main/kotlin/ru/resodostudios/cashsense/feature/subscriptions/SubscriptionsScreen.kt
+++ feature/subscriptions/src/main/kotlin/ru/resodostudios/cashsense/feature/subscriptions/SubscriptionsScreen.kt
@@ -1,21 +1,45 @@
 package ru.resodostudios.cashsense.feature.subscriptions
 
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import ru.resodostudios.cashsense.core.ui.EmptyState
+import ru.resodostudios.cashsense.core.ui.LoadingState
 
 @Composable
 internal fun SubscriptionsRoute(
-
+    viewModel: SubscriptionsViewModel = hiltViewModel()
 ) {
-    SubscriptionsScreen()
+    val subscriptionsState by viewModel.subscriptionsUiState.collectAsStateWithLifecycle()
+    SubscriptionsScreen(
+        subscriptionsState = subscriptionsState
+    )
 }
 
 @Composable
 internal fun SubscriptionsScreen(
-
+    subscriptionsState: SubscriptionsUiState
 ) {
-    EmptyState(
-        messageId = R.string.feature_subscriptions_empty_message,
-        animationId = R.raw.anim_empty_state
-    )
+    when (subscriptionsState) {
+        SubscriptionsUiState.Loading -> LoadingState()
+        is SubscriptionsUiState.Success -> {
+            if (subscriptionsState.subscriptions.isNotEmpty()) {
+                LazyVerticalGrid(
+                    columns = GridCells.Adaptive(300.dp)
+                ) {
+
+                }
+            } else {
+                EmptyState(
+                    messageId = R.string.feature_subscriptions_empty_message,
+                    animationId = R.raw.anim_empty_state
+                )
+            }
+        }
+    }
+
 }
