459aed8eda6df27d5134dd7dddfcfcb14e8ee862
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-06 13:48:13 +0300

    Add OfflineSubscriptionsRepository

---
+++ core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/repository/offline/OfflineSubscriptionsRepository.kt
@@ -0,0 +1,28 @@
+package ru.resodostudios.cashsense.core.data.repository.offline
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import ru.resodostudios.cashsense.core.data.model.asEntity
+import ru.resodostudios.cashsense.core.data.repository.SubscriptionsRepository
+import ru.resodostudios.cashsense.core.database.dao.SubscriptionDao
+import ru.resodostudios.cashsense.core.database.model.SubscriptionEntity
+import ru.resodostudios.cashsense.core.database.model.asExternalModel
+import ru.resodostudios.cashsense.core.model.data.Subscription
+import java.util.UUID
+import javax.inject.Inject
+
+class OfflineSubscriptionsRepository @Inject constructor(
+    private val subscriptionDao: SubscriptionDao
+) : SubscriptionsRepository {
+    override fun getSubscription(subscriptionId: UUID): Flow<Subscription> =
+        subscriptionDao.getSubscriptionEntity(subscriptionId).map { it.asExternalModel() }
+
+    override fun getSubscriptions(): Flow<List<Subscription>> =
+        subscriptionDao.getSubscriptionEntities().map { it.map(SubscriptionEntity::asExternalModel) }
+
+    override suspend fun upsertSubscription(subscription: Subscription) =
+        subscriptionDao.upsertSubscription(subscription.asEntity())
+
+    override suspend fun deleteSubscription(subscription: Subscription) =
+        subscriptionDao.deleteSubscription(subscription.asEntity())
+}
