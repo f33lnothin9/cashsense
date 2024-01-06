e631337a5534bc279bf44644429a0df8c2cca5c5
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-06 14:24:48 +0300

    Add SubscriptionsViewModel

---
+++ feature/subscriptions/src/main/kotlin/ru/resodostudios/cashsense/feature/subscriptions/SubscriptionsViewModel.kt
@@ -0,0 +1,58 @@
+package ru.resodostudios.cashsense.feature.subscriptions
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import ru.resodostudios.cashsense.core.data.repository.SubscriptionsRepository
+import ru.resodostudios.cashsense.core.model.data.Subscription
+import javax.inject.Inject
+
+@HiltViewModel
+class SubscriptionsViewModel @Inject constructor(
+    private val subscriptionsRepository: SubscriptionsRepository
+) : ViewModel() {
+
+    val subscriptionsUiState: StateFlow<SubscriptionsUiState> =
+        subscriptionsRepository.getSubscriptions()
+            .map<List<Subscription>, SubscriptionsUiState>(SubscriptionsUiState::Success)
+            .onStart { emit(SubscriptionsUiState.Loading) }
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = SubscriptionsUiState.Loading
+            )
+
+    fun upsertSubscription(subscription: Subscription) {
+        viewModelScope.launch {
+            subscriptionsRepository.upsertSubscription(subscription)
+        }
+    }
+
+    fun deleteSubscription(subscription: Subscription) {
+        viewModelScope.launch {
+            subscriptionsRepository.deleteSubscription(subscription)
+        }
+    }
+}
+
+sealed interface SubscriptionsUiState {
+    data object Loading : SubscriptionsUiState
+
+    data class Success(
+        val subscriptions: List<Subscription>
+    ) : SubscriptionsUiState
+}
+
+sealed interface SubscriptionUiState {
+    data object Loading : SubscriptionUiState
+
+    data class Success(
+        val subscription: Subscription
+    ) : SubscriptionUiState
+}
