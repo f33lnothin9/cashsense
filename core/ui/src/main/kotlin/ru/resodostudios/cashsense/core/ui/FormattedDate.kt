03681084fb5d9fd03be837ca136d05e39a00d7ae
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-08 13:26:20 +0300

    Move FormattedDate to core:ui

---
+++ core/ui/src/main/kotlin/ru/resodostudios/cashsense/core/ui/FormattedDate.kt
@@ -0,0 +1,38 @@
+package ru.resodostudios.cashsense.core.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import kotlinx.datetime.Instant
+import kotlinx.datetime.toJavaInstant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.Locale
+
+@Composable
+fun FormattedDate(date: Instant): String {
+    var zoneId by remember { mutableStateOf(ZoneId.systemDefault()) }
+
+    val context = LocalContext.current
+
+    DisposableEffect(context) {
+        val receiver = TimeZoneBroadcastReceiver(
+            onTimeZoneChanged = { zoneId = ZoneId.systemDefault() },
+        )
+        receiver.register(context)
+        onDispose {
+            receiver.unregister(context)
+        }
+    }
+
+    return DateTimeFormatter
+        .ofLocalizedDate(FormatStyle.MEDIUM)
+        .withLocale(Locale.getDefault())
+        .withZone(zoneId)
+        .format(date.toJavaInstant())
+}
--- feature/transaction/src/main/kotlin/ru/resodostudios/cashsense/feature/transaction/TransactionsList.kt
+++ feature/transaction/src/main/kotlin/ru/resodostudios/cashsense/feature/transaction/TransactionsList.kt
@@ -7,29 +7,16 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import kotlinx.datetime.Instant
-import kotlinx.datetime.toJavaInstant
 import ru.resodostudios.cashsense.core.model.data.Currency
 import ru.resodostudios.cashsense.core.model.data.Transaction
 import ru.resodostudios.cashsense.core.model.data.TransactionWithCategory
 import ru.resodostudios.cashsense.core.ui.EditAndDeleteDropdownMenu
-import ru.resodostudios.cashsense.core.ui.TimeZoneBroadcastReceiver
+import ru.resodostudios.cashsense.core.ui.FormattedDate
 import ru.resodostudios.cashsense.core.ui.getFormattedAmountAndCurrency
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-import java.time.format.FormatStyle
-import java.util.Locale
 import ru.resodostudios.cashsense.core.ui.R as uiR
 
 fun LazyListScope.transactions(
@@ -43,8 +30,8 @@ fun LazyListScope.transactions(
 
     items(sortedTransactions) { transactionWithCategory ->
         Column {
-            if (currentDate != dateFormatted(date = transactionWithCategory.transaction.date)) {
-                currentDate = dateFormatted(date = transactionWithCategory.transaction.date)
+            if (currentDate != FormattedDate(date = transactionWithCategory.transaction.date)) {
+                currentDate = FormattedDate(date = transactionWithCategory.transaction.date)
                 Text(
                     text = currentDate!!,
                     style = MaterialTheme.typography.labelLarge,
@@ -76,27 +63,4 @@ fun LazyListScope.transactions(
             )
         }
     }
-}
-
-@Composable
-private fun dateFormatted(date: Instant): String {
-    var zoneId by remember { mutableStateOf(ZoneId.systemDefault()) }
-
-    val context = LocalContext.current
-
-    DisposableEffect(context) {
-        val receiver = TimeZoneBroadcastReceiver(
-            onTimeZoneChanged = { zoneId = ZoneId.systemDefault() },
-        )
-        receiver.register(context)
-        onDispose {
-            receiver.unregister(context)
-        }
-    }
-
-    return DateTimeFormatter
-        .ofLocalizedDate(FormatStyle.MEDIUM)
-        .withLocale(Locale.getDefault())
-        .withZone(zoneId)
-        .format(date.toJavaInstant())
 }
