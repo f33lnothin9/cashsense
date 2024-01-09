cb9dd510b27c6bc69578946b0d000278611b2e39
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-09 22:37:53 +0300

    Fill the subscriptions screen to max size

--- feature/subscriptions/src/main/kotlin/ru/resodostudios/cashsense/feature/subscriptions/SubscriptionsScreen.kt
+++ feature/subscriptions/src/main/kotlin/ru/resodostudios/cashsense/feature/subscriptions/SubscriptionsScreen.kt
@@ -1,5 +1,6 @@
 package ru.resodostudios.cashsense.feature.subscriptions
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -7,6 +8,7 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -33,7 +35,8 @@ internal fun SubscriptionsScreen(
         is SubscriptionsUiState.Success -> {
             if (subscriptionsState.subscriptions.isNotEmpty()) {
                 LazyVerticalGrid(
-                    columns = GridCells.Adaptive(300.dp)
+                    columns = GridCells.Adaptive(300.dp),
+                    modifier = Modifier.fillMaxSize()
                 ) {
                     items(subscriptionsState.subscriptions) { subscription ->
                         ListItem(
