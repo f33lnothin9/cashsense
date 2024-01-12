a7cd977fea63dc5f27fac900587abe7e0e3789b1
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-12 08:49:34 +0300

    Fix validation

--- core/ui/src/main/kotlin/ru/resodostudios/cashsense/core/ui/Validation.kt
+++ core/ui/src/main/kotlin/ru/resodostudios/cashsense/core/ui/Validation.kt
@@ -1,19 +1,19 @@
 package ru.resodostudios.cashsense.core.ui
 
 fun validateAmount(input: String): Pair<String, Boolean> {
-    var cleanedInput = input.replace(".", ",")
-    val parts = cleanedInput.split(",")
+    var cleanedInput = input.replace(",", ".")
+    val parts = cleanedInput.split(".")
 
     val validInput = if (parts.size > 1) {
         val decimalPart = parts[1].substring(0, minOf(parts[1].length, 2))
-        "${parts[0]},$decimalPart"
+        "${parts[0]}.$decimalPart"
     } else {
         cleanedInput
     }
 
     cleanedInput = validInput.replace("^--".toRegex(), "-")
 
-    val regex = Regex("^-?\\d+(,\\d{0,2})?$")
+    val regex = Regex("^-?\\d+(\\.\\d{1,2})?$")
     val isValid = regex.matches(cleanedInput)
 
     return Pair(cleanedInput, isValid)
