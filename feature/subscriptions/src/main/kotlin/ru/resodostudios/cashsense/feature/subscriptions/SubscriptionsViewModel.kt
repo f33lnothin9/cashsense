44ae21e4cfbd8ba49cbdd946711d07d7d93a8ae9
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-13 11:53:13 +0300

    Add subscriptionUiState

--- feature/subscriptions/src/main/kotlin/ru/resodostudios/cashsense/feature/subscriptions/SubscriptionsViewModel.kt
+++ feature/subscriptions/src/main/kotlin/ru/resodostudios/cashsense/feature/subscriptions/SubscriptionsViewModel.kt
@@ -1,5 +1,6 @@
 package ru.resodostudios.cashsense.feature.subscriptions
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,12 +12,29 @@ import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import ru.resodostudios.cashsense.core.data.repository.SubscriptionsRepository
 import ru.resodostudios.cashsense.core.model.data.Subscription
+import ru.resodostudios.cashsense.feature.subscriptions.navigation.SubscriptionArgs
+import java.util.UUID
 import javax.inject.Inject
 
 @HiltViewModel
 class SubscriptionsViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
     private val subscriptionsRepository: SubscriptionsRepository
 ) : ViewModel() {
+
+    private val subscriptionArgs: SubscriptionArgs = SubscriptionArgs(savedStateHandle)
+
+    val subscriptionId = subscriptionArgs.subscriptionId
+
+    val subscriptionUiState: StateFlow<SubscriptionUiState> =
+        subscriptionsRepository.getSubscription(UUID.fromString(subscriptionId))
+        .map<Subscription, SubscriptionUiState>(SubscriptionUiState::Success)
+        .onStart { emit(SubscriptionUiState.Loading) }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = SubscriptionUiState.Loading
+        )
 
     val subscriptionsUiState: StateFlow<SubscriptionsUiState> =
         subscriptionsRepository.getSubscriptions()
