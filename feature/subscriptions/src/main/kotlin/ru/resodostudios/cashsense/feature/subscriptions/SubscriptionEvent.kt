3cd461b105ae3c0866f025fb7c9d45483a17d8bd
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-15 22:14:44 +0300

    Add SubscriptionEvent

---
+++ feature/subscriptions/src/main/kotlin/ru/resodostudios/cashsense/feature/subscriptions/SubscriptionEvent.kt
@@ -0,0 +1,9 @@
+package ru.resodostudios.cashsense.feature.subscriptions
+
+sealed interface SubscriptionEvent {
+    data class UpdateTitle(val title: String) : SubscriptionEvent
+    data class UpdateAmount(val amount: String) : SubscriptionEvent
+    data class UpdatePaymentDate(val paymentDate: String) : SubscriptionEvent
+    data class UpdateCurrency(val currency: String) : SubscriptionEvent
+    data object Confirm : SubscriptionEvent
+}
