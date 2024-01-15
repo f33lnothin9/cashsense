6a1532167f7443c0429e5b029c4914cde5cb7b8f
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-15 22:33:48 +0300

    Add SubscriptionState

---
+++ feature/subscriptions/src/main/kotlin/ru/resodostudios/cashsense/feature/subscriptions/SubscriptionState.kt
@@ -0,0 +1,8 @@
+package ru.resodostudios.cashsense.feature.subscriptions
+
+data class SubscriptionState(
+    val title: String = "",
+    val amount: String = "",
+    val paymentDate: String = "",
+    val currency: String = ""
+)
