91a75e774fd478e55b2db11c4fbf0097a3929fd4
Nikita Bulavin <f33lnothin9@proton.me>
2024-01-30 22:18:17 +0300

    Remove windowSizeClass & windowManager libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,10 +70,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
-    implementation(libs.androidx.compose.material3.windowSizeClass)
     implementation(libs.androidx.lifecycle.runtimeCompose)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.window.manager)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.datetime)
 
