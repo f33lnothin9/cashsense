04742f43b1a28977f3e3b41575e5628150cddae4
Nikita Bulavin <f33lnothin9@proton.me>
2024-02-01 08:51:50 +0300

    Add TransactionEvent

--- feature/transaction/src/main/kotlin/ru/resodostudios/cashsense/feature/transaction/TransactionUiState.kt
+++ feature/transaction/src/main/kotlin/ru/resodostudios/cashsense/feature/transaction/TransactionUiState.kt
@@ -1,9 +1,12 @@
 package ru.resodostudios.cashsense.feature.transaction
 
+import ru.resodostudios.cashsense.core.model.data.Category
+
 data class TransactionUiState(
     val walletOwnerId: String = "",
     val description: String = "",
     val amount: String = "",
     val date: String = "",
-    val time: String = ""
+    val time: String = "",
+    val category: Category = Category()
 )
