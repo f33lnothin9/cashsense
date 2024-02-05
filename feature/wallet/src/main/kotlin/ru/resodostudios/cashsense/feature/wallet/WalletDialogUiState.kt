3d046bbf75457addbe308b03dc0570dedf24d583
Nikita Bulavin <f33lnothin9@proton.me>
2024-02-05 19:35:23 +0300

    Add WalletDialogViewModel

--- feature/wallet/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/WalletDialogUiState.kt
+++ feature/wallet/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/WalletDialogUiState.kt
@@ -2,7 +2,7 @@ package ru.resodostudios.cashsense.feature.wallet
 
 import ru.resodostudios.cashsense.core.model.data.Currency
 
-data class WalletItemUiState(
+data class WalletDialogUiState(
     val id: String = "",
     val title: String = "",
     val initialBalance: String = "",
