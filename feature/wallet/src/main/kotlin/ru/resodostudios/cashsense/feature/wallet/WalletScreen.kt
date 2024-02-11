5e13ecee6b7dffeb49ba220c1e6b79460544bbb9
Nikita Bulavin <f33lnothin9@proton.me>
2024-02-11 16:42:22 +0300

    Move transaction list to wallet screen

--- feature/wallet/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/WalletScreen.kt
+++ feature/wallet/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/WalletScreen.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -33,14 +36,18 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.datetime.toJavaInstant
+import kotlinx.datetime.toKotlinInstant
 import ru.resodostudios.cashsense.core.designsystem.icon.CsIcons
 import ru.resodostudios.cashsense.core.model.data.Transaction
 import ru.resodostudios.cashsense.core.model.data.TransactionWithCategory
+import ru.resodostudios.cashsense.core.ui.EditAndDeleteDropdownMenu
 import ru.resodostudios.cashsense.core.ui.EmptyState
 import ru.resodostudios.cashsense.core.ui.LoadingState
+import ru.resodostudios.cashsense.core.ui.formattedDate
 import ru.resodostudios.cashsense.core.ui.getFormattedAmountAndCurrency
-import ru.resodostudios.cashsense.core.ui.transactions
 import ru.resodostudios.cashsense.feature.transaction.TransactionViewModel
+import java.time.temporal.ChronoUnit
 import ru.resodostudios.cashsense.feature.transaction.R as transactionR
 
 @Composable
@@ -204,6 +211,76 @@ private fun FinanceSection(
                 text = stringResource(R.string.feature_wallet_income),
                 modifier = Modifier.padding(start = 16.dp, bottom = 16.dp),
                 style = MaterialTheme.typography.bodyLarge,
+            )
+        }
+    }
+}
+
+fun LazyGridScope.transactions(
+    transactionsWithCategories: List<TransactionWithCategory>,
+    currency: String,
+    onEdit: (String) -> Unit,
+    onDelete: (Transaction) -> Unit,
+) {
+    val sortedTransactionsAndCategories =
+        transactionsWithCategories
+            .sortedByDescending { it.transaction.date }
+            .groupBy { it.transaction.date.toJavaInstant().truncatedTo(ChronoUnit.DAYS) }
+            .toSortedMap(compareByDescending { it })
+    val groupedTransactionsAndCategories = sortedTransactionsAndCategories.map {
+        Pair(
+            it.key,
+            it.value
+        )
+    }
+    groupedTransactionsAndCategories.forEach { group ->
+        item(
+            span = { GridItemSpan(maxLineSpan) }
+        ) {
+            Text(
+                text = formattedDate(group.first.toKotlinInstant()),
+                style = MaterialTheme.typography.labelLarge,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+        items(
+            items = group.second,
+            key = { it.transaction.id },
+            contentType = { "transactionWithCategory" }
+        ) { transactionWithCategory ->
+            val category = transactionWithCategory.category
+            ListItem(
+                headlineContent = {
+                    Text(
+                        text = getFormattedAmountAndCurrency(
+                            amount = transactionWithCategory.transaction.amount,
+                            currencyName = currency
+                        ),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                trailingContent = {
+                    EditAndDeleteDropdownMenu(
+                        onEdit = { onEdit(transactionWithCategory.transaction.id) },
+                        onDelete = { onDelete(transactionWithCategory.transaction) }
+                    )
+                },
+                supportingContent = {
+                    Text(
+                        text = category?.title ?: stringResource(ru.resodostudios.cashsense.core.ui.R.string.none),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                leadingContent = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(
+                            category?.iconRes ?: CsIcons.Transaction
+                        ),
+                        contentDescription = null
+                    )
+                }
             )
         }
     }
