dff24b98fec05aba32add70758bdaf8eb3e10c2a
Nikita Bulavin <f33lnothin9@proton.me>
2024-02-17 09:17:49 +0300

    Remove benchmark build type

--- build-logic/convention/src/main/kotlin/ru/resodostudios/cashsense/CsBuildType.kt
+++ build-logic/convention/src/main/kotlin/ru/resodostudios/cashsense/CsBuildType.kt
@@ -1,10 +1,6 @@
 package ru.resodostudios.cashsense
 
-/**
- * This is shared between :app and :benchmarks module to provide configurations type safety.
- */
 enum class CsBuildType(val applicationIdSuffix: String? = null) {
     DEBUG(".debug"),
     RELEASE,
-    BENCHMARK(".benchmark")
 }
