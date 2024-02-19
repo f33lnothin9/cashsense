0d3d4f5da9fedce32526e111aa85789eb3006a2d
Nikita Bulavin <f33lnothin9@proton.me>
2024-02-19 23:19:03 +0300

    Update compileSdk to VanillaIceCream

--- build-logic/convention/src/main/kotlin/ru/resodostudios/cashsense/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/ru/resodostudios/cashsense/KotlinAndroid.kt
@@ -17,7 +17,7 @@ internal fun Project.configureKotlinAndroid(
     commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
     commonExtension.apply {
-        compileSdk = 34
+        compileSdkPreview = "VanillaIceCream"
 
         defaultConfig {
             minSdk = 24
