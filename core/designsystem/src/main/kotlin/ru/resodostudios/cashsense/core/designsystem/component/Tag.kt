472ab7b18e8fc10ba52640a90b15039d0034772b
Nikita Bulavin <f33lnothin9@proton.me>
2024-02-26 23:07:13 +0300

    Add CsTag component

---
+++ core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/component/Tag.kt
@@ -0,0 +1,72 @@
+package ru.resodostudios.cashsense.core.designsystem.component
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import ru.resodostudios.cashsense.core.designsystem.theme.CsTheme
+
+@Composable
+fun CsTag(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.secondaryContainer,
+    shape: Shape = RoundedCornerShape(16.dp),
+    @DrawableRes
+    iconId: Int? = null,
+) {
+    Surface(
+        color = color,
+        shape = shape,
+        modifier = modifier,
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(
+                start = 8.dp,
+                top = 4.dp,
+                end = 8.dp,
+                bottom = 4.dp,
+            )
+        ) {
+            if (iconId != null) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(iconId),
+                    contentDescription = null,
+                    modifier = Modifier.size(18.dp),
+                )
+            }
+            Text(
+                text = text,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.labelLarge,
+            )
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun TagPreview() {
+    CsTheme {
+        CsTag(text = "TAG Tag")
+    }
+}
