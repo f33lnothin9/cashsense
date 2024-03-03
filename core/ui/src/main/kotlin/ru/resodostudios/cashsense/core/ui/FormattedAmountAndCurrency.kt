5803fab7ceab988e7f2537a2278d154f90e8066b
Nikita Bulavin <f33lnothin9@proton.me>
2024-03-03 09:43:43 +0300

    Remove trailing zeros

--- core/ui/src/main/kotlin/ru/resodostudios/cashsense/core/ui/FormattedAmountAndCurrency.kt
+++ core/ui/src/main/kotlin/ru/resodostudios/cashsense/core/ui/FormattedAmountAndCurrency.kt
@@ -1,18 +1,26 @@
 package ru.resodostudios.cashsense.core.ui
 
 import java.math.BigDecimal
-import java.text.NumberFormat
+import java.text.DecimalFormat
 import java.util.Currency
 import java.util.Locale
 
 fun BigDecimal.formatAmountWithCurrency(currency: String, withPlus: Boolean = false): String {
-    val currencyFormat = NumberFormat.getCurrencyInstance(Locale.getDefault())
+    val currencyFormat = DecimalFormat.getCurrencyInstance(Locale.getDefault())
     val customCurrency = Currency.getInstance(currency)
     currencyFormat.currency = customCurrency
 
-    return if (withPlus) {
-        "${if (this > BigDecimal(0)) "+" else ""}${currencyFormat.format(this)}"
+    val formattedAmount = currencyFormat.format(this)
+
+    val amountString = if (formattedAmount.contains('.')) {
+        formattedAmount.replace(Regex("\\.0+$"), "")
     } else {
-        currencyFormat.format(this)
+        formattedAmount
+    }
+
+    return if (withPlus) {
+        "${if (this > BigDecimal.ZERO) "+" else ""}$amountString"
+    } else {
+        amountString
     }
 }
