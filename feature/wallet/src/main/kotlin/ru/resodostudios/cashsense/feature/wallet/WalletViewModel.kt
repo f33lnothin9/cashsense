708672a110147df905dabb50e006d0c038c7a4e2
Nikita Bulavin <f33lnothin9@proton.me>
2024-03-08 16:05:37 +0300

    Add transactions filter by selected categories

--- feature/wallet/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/WalletViewModel.kt
+++ feature/wallet/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/WalletViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.catch
@@ -11,6 +12,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import ru.resodostudios.cashsense.core.data.repository.WalletsRepository
+import ru.resodostudios.cashsense.core.model.data.Category
 import ru.resodostudios.cashsense.core.model.data.WalletWithTransactionsAndCategories
 import ru.resodostudios.cashsense.feature.wallet.navigation.WalletArgs
 import javax.inject.Inject
@@ -23,15 +25,31 @@ class WalletViewModel @Inject constructor(
 
     private val walletArgs: WalletArgs = WalletArgs(savedStateHandle)
 
+    private val selectedCategories = MutableStateFlow<List<Category>>(emptyList())
+
     val walletUiState: StateFlow<WalletUiState> =
         walletsRepository.getWalletWithTransactions(walletArgs.walletId)
-            .map<WalletWithTransactionsAndCategories, WalletUiState>(WalletUiState::Success)
+            .map<WalletWithTransactionsAndCategories, WalletUiState> { walletTransactionsCategories ->
+                val walletData = WalletWithTransactionsAndCategories(
+                    wallet = walletTransactionsCategories.wallet,
+                    transactionsWithCategories = if (selectedCategories.value.isNotEmpty()) {
+                        walletTransactionsCategories.transactionsWithCategories.filter { selectedCategories.value.contains(it.category) }
+                    } else {
+                        walletTransactionsCategories.transactionsWithCategories
+                    }
+                )
+
+                WalletUiState.Success(
+                    walletWithTransactionsAndCategories = walletData,
+                    selectedCategories = selectedCategories.value
+                )
+            }
             .onStart { emit(WalletUiState.Loading) }
             .catch { emit(WalletUiState.Loading) }
             .stateIn(
                 scope = viewModelScope,
                 started = SharingStarted.WhileSubscribed(5_000),
-                initialValue = WalletUiState.Loading
+                initialValue = WalletUiState.Loading,
             )
 }
 
@@ -40,6 +58,7 @@ sealed interface WalletUiState {
     data object Loading : WalletUiState
 
     data class Success(
-        val walletWithTransactionsAndCategories: WalletWithTransactionsAndCategories
+        val walletWithTransactionsAndCategories: WalletWithTransactionsAndCategories,
+        val selectedCategories: List<Category>,
     ) : WalletUiState
 }
