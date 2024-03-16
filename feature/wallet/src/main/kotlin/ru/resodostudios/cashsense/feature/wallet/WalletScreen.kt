25a7a2a80e2872c5dd910eef16d6f1a38cde839d
Nikita Bulavin <f33lnothin9@proton.me>
2024-03-16 08:27:06 +0300

    Fix stickyHeader paddings

--- feature/wallet/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/WalletScreen.kt
+++ feature/wallet/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/WalletScreen.kt
@@ -474,7 +474,7 @@ private fun LazyListScope.transactions(
                     .formatDate(),
                 modifier = Modifier
                     .animateItemPlacement()
-                    .padding(16.dp),
+                    .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp),
             )
         }
         items(
