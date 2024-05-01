3a3671fa02dd7d66419839ca01450005e9208db2
Nikita Bulavin <f33lnothin9@proton.me>
2024-05-01 15:38:33 +0300

    Add second font

--- core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/theme/Type.kt
+++ core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/theme/Type.kt
@@ -12,16 +12,20 @@ import androidx.compose.ui.text.style.LineHeightStyle.Trim
 import androidx.compose.ui.unit.sp
 import ru.resodostudios.cashsense.core.designsystem.R
 
-internal val provider = GoogleFont.Provider(
+internal val fontProvider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
     providerPackage = "com.google.android.gms",
-    certificates = R.array.com_google_android_gms_fonts_certs
+    certificates = R.array.com_google_android_gms_fonts_certs,
 )
 
-internal val fontName = GoogleFont("Roboto Slab")
+internal val titleFontName = GoogleFont("Roboto Slab")
+internal val bodyFontName = GoogleFont("Poppins")
 
-internal val fontFamily = FontFamily(
-    Font(googleFont = fontName, fontProvider = provider)
+internal val titleFontFamily = FontFamily(
+    Font(googleFont = titleFontName, fontProvider = fontProvider)
+)
+internal val bodyFontFamily = FontFamily(
+    Font(googleFont = bodyFontName, fontProvider = fontProvider)
 )
 
 internal val CsTypography = Typography(
@@ -30,61 +34,63 @@ internal val CsTypography = Typography(
         fontSize = 57.sp,
         lineHeight = 64.sp,
         letterSpacing = (-0.25).sp,
-        fontFamily = fontFamily
+        fontFamily = titleFontFamily,
     ),
     displayMedium = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 45.sp,
         lineHeight = 52.sp,
         letterSpacing = 0.sp,
-        fontFamily = fontFamily
+        fontFamily = titleFontFamily,
     ),
     displaySmall = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
         lineHeight = 44.sp,
         letterSpacing = 0.sp,
-        fontFamily = fontFamily
+        fontFamily = titleFontFamily,
     ),
     headlineLarge = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp,
-        fontFamily = fontFamily
+        fontFamily = titleFontFamily,
     ),
     headlineMedium = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp,
-        fontFamily = fontFamily
+        fontFamily = titleFontFamily,
     ),
     headlineSmall = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp,
-        fontFamily = fontFamily
+        fontFamily = titleFontFamily,
     ),
     titleLarge = TextStyle(
         fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp,
-        fontFamily = fontFamily
+        fontFamily = titleFontFamily,
     ),
     titleMedium = TextStyle(
         fontWeight = FontWeight.Bold,
         fontSize = 18.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.1.sp
+        letterSpacing = 0.1.sp,
+        fontFamily = bodyFontFamily,
     ),
     titleSmall = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.1.sp
+        letterSpacing = 0.1.sp,
+        fontFamily = bodyFontFamily,
     ),
     bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
@@ -95,35 +101,41 @@ internal val CsTypography = Typography(
             alignment = Alignment.Center,
             trim = Trim.None,
         ),
+        fontFamily = bodyFontFamily,
     ),
     bodyMedium = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.25.sp
+        letterSpacing = 0.25.sp,
+        fontFamily = bodyFontFamily,
     ),
     bodySmall = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.4.sp
+        letterSpacing = 0.4.sp,
+        fontFamily = bodyFontFamily,
     ),
     labelLarge = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
-        letterSpacing = 0.1.sp
+        letterSpacing = 0.1.sp,
+        fontFamily = bodyFontFamily,
     ),
     labelMedium = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = 0.5.sp,
+        fontFamily = bodyFontFamily,
     ),
     labelSmall = TextStyle(
         fontWeight = FontWeight.Medium,
         fontSize = 10.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
+        fontFamily = bodyFontFamily,
     )
 )
