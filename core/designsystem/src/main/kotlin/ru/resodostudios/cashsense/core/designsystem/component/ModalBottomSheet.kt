48dd07f03af4de83dc7b04411190b1b3c32fdff3
Nikita Bulavin <f33lnothin9@proton.me>
2024-05-02 09:40:25 +0300

    Remove window insets

--- core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/component/ModalBottomSheet.kt
+++ core/designsystem/src/main/kotlin/ru/resodostudios/cashsense/core/designsystem/component/ModalBottomSheet.kt
@@ -2,8 +2,6 @@ package ru.resodostudios.cashsense.core.designsystem.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,13 +23,8 @@ fun CsModalBottomSheet(
     ModalBottomSheet(
         onDismissRequest = { onDismiss() },
         sheetState = sheetState,
-        windowInsets = WindowInsets(0, 0, 0, 0),
     ) {
-        Column(
-            modifier = Modifier
-                .verticalScroll(rememberScrollState())
-                .navigationBarsPadding(),
-        ) {
+        Column(Modifier.verticalScroll(rememberScrollState())) {
             content()
         }
     }
