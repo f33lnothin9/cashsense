4b460bfe6948b66dcc37f8cbb273b4861d1d503d
Nikita Bulavin <f33lnothin9@proton.me>
2024-05-13 13:05:11 +0300

    Add allow notifications action to startup profile

---
+++ baselineprofile/src/main/kotlin/ru/resodostudios/cashsense/GeneralActions.kt
@@ -0,0 +1,37 @@
+package ru.resodostudios.cashsense
+
+import android.Manifest.permission
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.TIRAMISU
+import androidx.benchmark.macro.MacrobenchmarkScope
+
+/**
+ * Because the app under test is different from the one running the instrumentation test,
+ * the permission has to be granted manually by either:
+ *
+ * - tapping the Allow button
+ *    ```kotlin
+ *    val obj = By.text("Allow")
+ *    val dialog = device.wait(Until.findObject(obj), TIMEOUT)
+ *    dialog?.let {
+ *        it.click()
+ *        device.wait(Until.gone(obj), 5_000)
+ *    }
+ *    ```
+ * - or (preferred) executing the grant command on the target package.
+ */
+fun MacrobenchmarkScope.allowNotifications() {
+    if (SDK_INT >= TIRAMISU) {
+        val command = "pm grant $packageName ${permission.POST_NOTIFICATIONS}"
+        device.executeShellCommand(command)
+    }
+}
+
+/**
+ * Wraps starting the default activity, waiting for it to start and then allowing notifications in
+ * one convenient call.
+ */
+fun MacrobenchmarkScope.startActivityAndAllowNotifications() {
+    startActivityAndWait()
+    allowNotifications()
+}
