8a44fbf464a13ef059d1a1ac84e5383027771327
Nikita Bulavin <f33lnothin9@proton.me>
2024-05-15 14:36:20 +0300

    Add reschedule behavior after rebooting device

--- core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/util/AlarmReceiver.kt
+++ core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/util/AlarmReceiver.kt
@@ -3,13 +3,38 @@ package ru.resodostudios.cashsense.core.data.util
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.resodostudios.cashsense.core.data.repository.SubscriptionsRepository
+import javax.inject.Inject
 
-class AlarmReceiver : BroadcastReceiver() {
+@AndroidEntryPoint
+internal class AlarmReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var subscriptionsRepository: SubscriptionsRepository
+
+    @Inject
+    lateinit var notificationAlarmScheduler: NotificationAlarmScheduler
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        println("Notification triggered")
+
+        val reminderId = intent?.getIntExtra(EXTRA_REMINDER_ID, 0)
+
         if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            println("Device booted")
+            CoroutineScope(Dispatchers.IO).launch {
+                subscriptionsRepository.getSubscriptions().collect { subscriptions ->
+                    subscriptions
+                        .filter { it.reminder != null }
+                        .forEach { subscription ->
+                            subscription.reminder?.let {
+                                notificationAlarmScheduler.schedule(it)
+                            }
+                        }
+                }
+            }
         }
     }
 }
