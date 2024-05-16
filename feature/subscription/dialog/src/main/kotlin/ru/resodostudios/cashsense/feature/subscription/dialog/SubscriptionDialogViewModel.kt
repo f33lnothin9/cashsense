e4421264492333855e3d4de43f5481c647f1decc
Nikita Bulavin <f33lnothin9@proton.me>
2024-05-16 12:38:06 +0300

    Setup notification date

--- feature/subscription/dialog/src/main/kotlin/ru/resodostudios/cashsense/feature/subscription/dialog/SubscriptionDialogViewModel.kt
+++ feature/subscription/dialog/src/main/kotlin/ru/resodostudios/cashsense/feature/subscription/dialog/SubscriptionDialogViewModel.kt
@@ -1,5 +1,6 @@
 package ru.resodostudios.cashsense.feature.subscription.dialog
 
+import android.app.AlarmManager
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -10,7 +11,14 @@ import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 import ru.resodostudios.cashsense.core.data.repository.SubscriptionsRepository
 import ru.resodostudios.cashsense.core.model.data.Currency
 import ru.resodostudios.cashsense.core.model.data.Reminder
@@ -30,6 +38,14 @@ class SubscriptionDialogViewModel @Inject constructor(
         when (event) {
             SubscriptionDialogEvent.Save -> {
                 val subscriptionId = _subscriptionDialogUiState.value.id.ifEmpty { UUID.randomUUID().toString() }
+
+                val timeZone = TimeZone.currentSystemDefault()
+                val currentInstant = _subscriptionDialogUiState.value.paymentDate
+                val currentDateTime = currentInstant.toLocalDateTime(timeZone)
+                val previousDate = currentDateTime.date.minus(1, DateTimeUnit.DAY)
+                val previousDayAtNineAm = LocalDateTime(previousDate, LocalTime(9, 0))
+                    .toInstant(timeZone)
+
                 val subscription = Subscription(
                     id = subscriptionId,
                     title = _subscriptionDialogUiState.value.title,
@@ -38,8 +54,8 @@ class SubscriptionDialogViewModel @Inject constructor(
                     currency = _subscriptionDialogUiState.value.currency,
                     reminder = Reminder(
                         id = subscriptionId.hashCode(),
-                        notificationDate = Instant.fromEpochMilliseconds(Clock.System.now().toEpochMilliseconds() + 10000L),
-                        repeatingInterval = 15000L,
+                        notificationDate = previousDayAtNineAm,
+                        repeatingInterval = 30 * AlarmManager.INTERVAL_DAY,
                     ),
                 )
                 viewModelScope.launch {
