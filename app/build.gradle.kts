b1fe5b6c2db96bb64dd909c819652f02d5b39814
Nikita Bulavin <f33lnothin9@proton.me>
2024-05-19 21:08:30 +0300

    Update app version to 0.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "ru.resodostudios.cashsense"
-        versionCode = 14
-        versionName = "0.4.1"
+        versionCode = 15
+        versionName = "0.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
