64ef0c7938720f51fdc9e1a568fbb68c2a663e74
Nikita Bulavin <f33lnothin9@proton.me>
2024-06-23 17:11:24 +0300

    Add WalletWidgetReceiver

---
+++ feature/wallet/widget/src/main/kotlin/ru/resodostudios/cashsense/wallet/widget/WalletWidgetReceiver.kt
@@ -0,0 +1,9 @@
+package ru.resodostudios.cashsense.wallet.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class WalletWidgetReceiver : GlanceAppWidgetReceiver() {
+
+    override val glanceAppWidget: GlanceAppWidget = TODO("Create WalletAppWidget")
+}
