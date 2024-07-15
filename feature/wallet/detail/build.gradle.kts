5bd9bd1128faee2c8c3086210148d54822b9013b
Nikita Bulavin <f33lnothin9@proton.me>
2024-07-15 22:19:21 +0300

    Add vico dependencies

--- feature/wallet/detail/build.gradle.kts
+++ feature/wallet/detail/build.gradle.kts
@@ -15,5 +15,7 @@ dependencies {
 
     implementation(libs.androidx.compose.material3.adaptive)
     implementation(libs.androidx.compose.material3.adaptive.navigation)
-    implementation(libs.ycharts)
+    implementation(libs.vico.compose)
+    implementation(libs.vico.compose.m3)
+    implementation(libs.vico.core)
 }
