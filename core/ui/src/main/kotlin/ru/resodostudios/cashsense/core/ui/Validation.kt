5517943ecdc39c8b90b04882f2019712e6b7ead1
Nikita Bulavin <f33lnothin9@proton.me>
2024-09-10 20:54:08 +0300

    Refactor cleanAndValidateAmount

--- core/ui/src/main/kotlin/ru/resodostudios/cashsense/core/ui/Validation.kt
+++ core/ui/src/main/kotlin/ru/resodostudios/cashsense/core/ui/Validation.kt
@@ -1,23 +1,11 @@
 package ru.resodostudios.cashsense.core.ui
 
-fun String.validateAmount(): Pair<String, Boolean> {
-    var cleanedInput = this.replace(",", ".")
-    cleanedInput = cleanedInput.replace("-", "")
-    val parts = cleanedInput.split(".")
+fun String.cleanAndValidateAmount(): Pair<String, Boolean> {
+    if (isEmpty()) return "" to false // Handle empty strings
 
-    cleanedInput = if (parts.size > 1) {
-        val decimalPart = parts[1].substring(0, minOf(parts[1].length, 2))
-        "${parts[0]}.$decimalPart"
-    } else {
-        cleanedInput
-    }
+    val regex = Regex("^\\d+(\\.\\d{0,2})?") // Match integers and decimals up to 2 digits
+    val matchResult = regex.find(this.replace(",", ".")) // Directly match and replace comma
 
-    if (cleanedInput.toDoubleOrNull() == 0.0) {
-        return cleanedInput to false
-    }
-
-    val regex = Regex("^\\d+(\\.\\d{1,2})?$")
-    val isValid = regex.matches(cleanedInput)
-
-    return cleanedInput to isValid
+    val cleanedInput = matchResult?.value ?: ""
+    return cleanedInput to (cleanedInput.isNotEmpty())
 }
