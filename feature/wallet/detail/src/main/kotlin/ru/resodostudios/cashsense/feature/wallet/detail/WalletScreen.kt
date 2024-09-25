8e52e14cf768ee0bba08e2ac8f46355e080e49e3
Nikita Bulavin <f33lnothin9@proton.me>
2024-09-25 21:23:51 +0300

    Add min size for date type filter

--- feature/wallet/detail/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/detail/WalletScreen.kt
+++ feature/wallet/detail/src/main/kotlin/ru/resodostudios/cashsense/feature/wallet/detail/WalletScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -587,6 +588,9 @@ private fun SharedTransitionScope.DetailedFinanceSection(
             FilterDateTypeSelectorRow(
                 walletFilter = walletFilter,
                 onWalletEvent = onWalletEvent,
+                modifier = Modifier
+                    .defaultMinSize(minWidth = 400.dp)
+                    .weight(1f, false),
             )
             FilledTonalIconButton(
                 onClick = onBackClick,
