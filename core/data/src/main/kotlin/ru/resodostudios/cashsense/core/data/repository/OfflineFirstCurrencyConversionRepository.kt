8259b6effc9db2403d9e18750a3bfb001df3cb8b
Nikita Bulavin <f33lnothin9@proton.me>
2025-01-08 19:41:03 +0300

    Remove `ioDispatcher`

--- core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/repository/OfflineFirstCurrencyConversionRepository.kt
+++ core/data/src/main/kotlin/ru/resodostudios/cashsense/core/data/repository/OfflineFirstCurrencyConversionRepository.kt
@@ -1,54 +1,53 @@
 package ru.resodostudios.cashsense.core.data.repository
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import ru.resodostudios.cashsense.core.data.model.asEntity
-import ru.resodostudios.cashsense.core.data.model.asExternalModel
 import ru.resodostudios.cashsense.core.database.dao.CurrencyConversionDao
 import ru.resodostudios.cashsense.core.database.model.asExternalModel
 import ru.resodostudios.cashsense.core.model.data.CurrencyExchangeRate
-import ru.resodostudios.cashsense.core.network.CsDispatchers.IO
 import ru.resodostudios.cashsense.core.network.CsNetworkDataSource
-import ru.resodostudios.cashsense.core.network.Dispatcher
 import java.util.Currency
 import javax.inject.Inject
 
 internal class OfflineFirstCurrencyConversionRepository @Inject constructor(
     private val dao: CurrencyConversionDao,
     private val network: CsNetworkDataSource,
-    @Dispatcher(IO) private val ioDispatcher: CoroutineDispatcher,
 ) : CurrencyConversionRepository {
 
     override fun getConvertedCurrencies(
         baseCurrencies: Set<Currency>,
         targetCurrency: Currency,
-    ): Flow<List<CurrencyExchangeRate>> = flow {
-        val localData = dao.getCurrencyExchangeRateEntities(
+    ): Flow<List<CurrencyExchangeRate>> =
+        dao.getCurrencyExchangeRateEntities(
             targetCurrency = targetCurrency,
             baseCurrencies = baseCurrencies,
-        ).firstOrNull()
-
-        if (!localData.isNullOrEmpty()) {
-            emit(localData.map { it.asExternalModel() })
-        } else {
-            val remoteData = try {
-                baseCurrencies.map { baseCurrency ->
-                    network.getCurrencyExchangeRate(
-                        baseCurrency.currencyCode,
-                        targetCurrency.currencyCode,
-                    )
+        )
+            .map { currencyExchangeRateCached ->
+                currencyExchangeRateCached.map { it.asExternalModel() }
+            }
+            .onEach { currencyExchangeRates ->
+                if (currencyExchangeRates.isEmpty() || currencyExchangeRates.size < baseCurrencies.size) {
+                    try {
+                        getCurrencyExchangeRates(baseCurrencies, targetCurrency)
+                    } catch (e: Exception) {
+                        emptyList()
+                    }
                 }
-            } catch (e: Exception) {
-                emit(emptyList())
-                null
             }
-            if (!remoteData.isNullOrEmpty()) {
-                dao.upsertCurrencyExchangeRates(remoteData.map { it.asEntity() })
-                emit(remoteData.map { it.asExternalModel() })
-            }
+
+    private suspend fun getCurrencyExchangeRates(
+        baseCurrencies: Set<Currency>,
+        targetCurrency: Currency,
+    ) = baseCurrencies
+        .map { baseCurrency ->
+            network.getCurrencyExchangeRate(
+                baseCurrencyCode = baseCurrency.currencyCode,
+                targetCurrencyCode = targetCurrency.currencyCode,
+            )
         }
-    }.flowOn(ioDispatcher)
+        .also { remoteData ->
+            dao.upsertCurrencyExchangeRates(remoteData.map { it.asEntity() })
+        }
 }
